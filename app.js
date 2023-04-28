77cd372569debff18246fcb2ba05867363805e6d
divya <divyaxdv@gmail.com>
2023-04-28 23:55:53 +0530

    toggle menu

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
-const App={
+const App= {
     $:{
-        act : document.querySelector('[data-id="menu"]');
-        menuBtn : document.querySelector('[data-id="menu-btn"]');
-        menuItems  : document.querySelector('[data-id="menu-items"]');
-        resetBtn: document.querySelector('[data-id="resetbtn"]');
-        newRoundBtn : document.querySelector('[data-id="nrbtn"]');
-        squares: document.querySelectorAll('[data-id="square"]');
+        menu: document.querySelector(".menu"),
+        menuItems: document.querySelector(".items"),
+    },
     init(){
-        App.$.menu.addEventListener("click",(event)=>{ 
-        App.$.menuItems.classList.toggle("hidden");
+        App.$.menu.addEventListener("click",(event)=>{
+            App.$.menuItems.classList.toggle("hidden");
         });
-
-        App.$.resetBtn.addEventListener("click",(event)=>{ 
-            console.log('reset the game')
-        });
-
-        App.$.newRoundBtn.addEventListener("click",(event)=>{ 
-            console.log('Add new round')
-        });
-
-        App.$.squares.forEach(square=>{ 
-            square.addEventListener('click', event=>{
-                console.log('Square with id ${event.target.id}was clicked');
-            });
-    });   
-},
-
-
+    }
 };
-
-window.addEventListener('load, App.init');
+window.addEventListener("load", App.init());
